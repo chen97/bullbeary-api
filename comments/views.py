55dff900741e3daf93fe8b171f7b107c22049f28
Chen Hao <chenhao.do@ufinity.com>
2021-03-03 22:33:26 +0800

    Enhance the comments app to have a sorted result. Sorting by parent comment and followed by child comments.

--- comments/views.py
+++ comments/views.py
@@ -7,7 +7,8 @@ from rest_framework import generics
 from rest_framework.permissions import AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly
 from BullbearyAPI.permissions import IsOwnerOrReadOnly
 from rest_framework.response import Response
-from rest_framework import mixins, views
+import operator
+
 
 class CommentList(generics.ListCreateAPIView):
     queryset = Comments.objects.all()
@@ -17,11 +18,26 @@ class CommentList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
+    # Customise to get comments based on post id, 
+    # for example localhost:8080/comments/?postid=1
     def get_queryset(self):
         queryset = Comments.objects.all()
+
+        # Getting query param and initialise it
         filter_value = self.request.query_params.get('postid', None)
+
+        # Filtering the query param from db result
         if filter_value is not None:
             queryset = queryset.filter(post_id=filter_value)
+
+        # Sorting the result
+        for comment in queryset:
+            if comment.parent_id is not None:
+                comment.newid = float(str(comment.parent_id)+"."+str(comment.id))
+            else:
+                comment.newid = float(str(comment.id)+".0")
+        queryset = sorted(queryset, key=operator.attrgetter('newid'))
+        
         return queryset
 
 class CommentDetail(generics.CreateAPIView, generics.RetrieveUpdateDestroyAPIView):
