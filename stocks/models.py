7cfda231891e018b2ff18982feae0238f2644dc0
Kah Hwa <whitehatbuds@gmail.com>
2021-03-09 00:11:02 +0800

    Change stocks

--- stocks/models.py
+++ stocks/models.py
@@ -1,20 +1,20 @@
-from django.db import models
+from django.db.models import Model, IntegerField, FloatField, CharField, BooleanField
 
 
-class Stocks(models.Model):
-    id = models.IntegerField(blank=False, primary_key=True)
-    description = models.CharField(max_length=200, blank=False)
-    published = models.CharField(max_length=70, blank=False)
-    cashtag = models.CharField(max_length=70, blank=False)
-    ticker = models.CharField(max_length=10, blank=True)
-    code = models.CharField(max_length=10, blank=True)
-    full_name = models.CharField(max_length=70, blank=False)
-    stock_price = models.FloatField(blank=False)
-    volume = models.FloatField(blank=False)
-    marketcap = models.FloatField(blank=False)
-    board = models.CharField(max_length=50, blank=True)
-    sector = models.CharField(max_length=50, blank=True)
-    is_shariah = models.BooleanField(blank=False)
+class Stocks(Model):
+    id = IntegerField(blank=False, primary_key=True)
+    description = CharField(max_length=200, blank=False)
+    published = CharField(max_length=70, blank=False)
+    cashtag = CharField(max_length=70, blank=False)
+    ticker = CharField(max_length=10, blank=True)
+    code = CharField(max_length=10, blank=True)
+    full_name = CharField(max_length=70, blank=False)
+    stock_price = FloatField(blank=False)
+    volume = FloatField(blank=False)
+    marketcap = FloatField(blank=False)
+    board = CharField(max_length=50, blank=True)
+    sector = CharField(max_length=50, blank=True)
+    is_shariah = BooleanField(blank=False)
 
     def __str__(self):
         return f'{self.full_name} {self.board}'
--- stocks/views.py
+++ stocks/views.py
@@ -3,17 +3,21 @@ from django.http.response import JsonResponse
 # from rest_framework import status
 from stocks.models import Stocks
 from stocks.serializers import StockSerializer
-from django.views.decorators.http import require_GET
+from django.views.decorators.http import require_safe
+
+# Web servers should automatically strip the content of responses to HEAD requests while leaving the headers unchanged,
+# so you may handle HEAD requests exactly like GET requests in your views. Since some software,
+# such as link checkers, rely on HEAD requests, you might prefer using require_safe instead of require_GET.
 
 
-@require_GET
+@require_safe
 def GetStocksFromBoard(request, board):
     q = Stocks.objects.filter(board=board)
     s = StockSerializer(q, many=True)
     return JsonResponse(s.data, safe=False)
 
 
-@require_GET
+@require_safe
 def GetStockDetails(request, board, code):
     q = Stocks.objects.filter(code=code).filter(board=board)
     s = StockSerializer(q, many=True)
